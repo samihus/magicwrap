a8c811883e7f898f61b3cbdee7fde9f8a1a1ead0
Samih <eljsam@yahoo.fr>
2019-09-13 18:44:45 +0200

    added types

--- test/com/samihus/magicdraw/wrapper/allTests/GeneralInformationTests.scala
+++ test/com/samihus/magicdraw/wrapper/allTests/GeneralInformationTests.scala
@@ -26,17 +26,17 @@ class GeneralInformationTests {
   }
 
   val emptyClass: BaseElement = project.getElementByID("_19_0_1_6490213_1568265874138_307782_4671")
-  val animal: BaseElement = project.getElementByID("_19_0_1_6490213_1566473579096_470275_4629")
-  val pet: BaseElement = project.getElementByID("_19_0_1_6490213_1566413767485_541943_4645")
-  val dog: BaseElement = project.getElementByID("_19_0_1_6490213_1566414121971_53324_4733")
-  val cat: BaseElement = project.getElementByID("_19_0_1_6490213_1566413850034_201781_4681")
-  val master: BaseElement = project.getElementByID("_19_0_1_6490213_1566414160102_998584_4773")
-  val friend: BaseElement = project.getElementByID("_19_0_1_6490213_1566473638161_142640_4660")
-  val enum1: BaseElement = project.getElementByID("_19_0_1_6490213_1567887734310_574362_4777")
-  val emptyData: BaseElement = project.getElementByID("_19_0_1_6490213_1567929782118_953555_4808")
-  val moveableI: BaseElement = project.getElementByID("_19_0_1_6490213_1566413913409_190065_4693")
-  val spacLocI: BaseElement = project.getElementByID("_19_0_1_6490213_1566474163273_634516_4694")
-  val param: BaseElement = project.getElementByID("_19_0_1_6490213_1568317061248_451752_4661")
+  val animal: BaseElement     = project.getElementByID("_19_0_1_6490213_1566473579096_470275_4629")
+  val pet: BaseElement        = project.getElementByID("_19_0_1_6490213_1566413767485_541943_4645")
+  val dog: BaseElement        = project.getElementByID("_19_0_1_6490213_1566414121971_53324_4733")
+  val cat: BaseElement        = project.getElementByID("_19_0_1_6490213_1566413850034_201781_4681")
+  val master: BaseElement     = project.getElementByID("_19_0_1_6490213_1566414160102_998584_4773")
+  val friend: BaseElement     = project.getElementByID("_19_0_1_6490213_1566473638161_142640_4660")
+  val enum1: BaseElement      = project.getElementByID("_19_0_1_6490213_1567887734310_574362_4777")
+  val emptyData: BaseElement  = project.getElementByID("_19_0_1_6490213_1567929782118_953555_4808")
+  val moveableI: BaseElement  = project.getElementByID("_19_0_1_6490213_1566413913409_190065_4693")
+  val spacLocI: BaseElement   = project.getElementByID("_19_0_1_6490213_1566474163273_634516_4694")
+  val param: BaseElement      = project.getElementByID("_19_0_1_6490213_1568317061248_451752_4661")
 
   //
   @Test def CastAndWrap = {
@@ -78,12 +78,5 @@ class GeneralInformationTests {
   @Test def testGetClassWithEmptyAtt = {
     assertNull(param)
   }
-
-
-
-
-
-
-
 
 }
